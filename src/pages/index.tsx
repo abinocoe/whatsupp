b6e6719ea5dc48db60451ac4266d4694f0bc77be
Abigail Coe <abigailcoe@gmail.com>
2019-05-16 16:04:35 +0100

    basic instructions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,13 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import FilePicker from "../components/filePicker"
+import Instructions from "../components/instructions"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`application`, `react`]} />
     <h1>Whatsupp</h1>
+    <Instructions />
     <FilePicker />
   </Layout>
 )
